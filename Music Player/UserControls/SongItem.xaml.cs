fc0215de7ef3a6af63f7a53291b30962eb62fc8a
hickee032 <hickee32@gmail.com>
2023-12-09 15:00:00 +0900

    Music Player

---
+++ Music Player/UserControls/SongItem.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Music_Player.UserControls
+{
+    /// <summary>
+    /// SongItem.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SongItem : UserControl
+    {
+        public SongItem() {
+            InitializeComponent();
+        }
+
+        #region Title
+        public string Title {
+            get { return (string)GetValue(TitleProperty);}
+            set { SetValue(TitleProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register
+            ("Title", typeof(string), typeof(SongItem));
+        #endregion
+
+        #region Number
+        public string Number {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register
+            ("Number", typeof(string), typeof(SongItem));
+        #endregion
+
+        #region Time
+        public string Time {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register
+            ("Time", typeof(string), typeof(SongItem));
+        #endregion
+
+        #region IsActive
+        public bool IsActive {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register
+            ("IsActive", typeof(bool), typeof(SongItem));
+        #endregion
+
+    }
+}
